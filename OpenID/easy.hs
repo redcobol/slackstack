fe54f6ac943222a04b0472efee2578380b1dd61a
o_O <substack@sunspot.(none)>
2009-10-20 23:02:49 -0800

    removed OpenID/ now that there's Network.OpenID.Easy

--- OpenID/easy.hs
+++
@@ -1,8 +0,0 @@
-import qualified OpenID.Easy as ID
-
-main = do
-    session <- ID.auth ID.config "substack.net" "http://sunspot:5050/"
-    putStrLn $ ID.sAuthURI session
-    putStr "forwarded back to uri: "
-    uri <- getLine
-    ID.verify session uri
