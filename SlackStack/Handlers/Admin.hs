89be8161b97ec78e1b79b67a34072423ad40abe2
o_O <substack@sunspot.(none)>
2009-10-23 11:31:56 -0800

    insert new posts with %F %T time

--- SlackStack/Handlers/Admin.hs
+++ SlackStack/Handlers/Admin.hs
@@ -10,17 +10,44 @@ import Control.Arrow
 import Control.Monad.Trans (liftIO)
 import Control.Monad
 
+import Data.Time.Clock (getCurrentTime)
+import Data.Time.Format
+import System.Locale (defaultTimeLocale)
+
 adminHandlers :: DB.IConnection conn =>
     Layout -> conn -> ServerPartT IO Response
-adminHandlers layout dbh = msum [
-        dir "admin" $ methodSP GET $ do
-            mIdentity <- getIdentity dbh
-            when (isNothing mIdentity) $
-                fail "This page requires identification"
-            
-            let (identity,level) = fromJust mIdentity
-            when (level /= Root) $
-                fail "Must be root-level to see this page"
-            
-            renderPage dbh layout "admin" []
-    ]
+adminHandlers layout dbh =
+    dir "admin" $ do
+        mIdentity <- getIdentity dbh
+        when (isNothing mIdentity) $
+            fail "This page requires identification"
+        let (identity,level) = fromJust mIdentity
+        when (level /= Root) $
+            fail "Must be root-level to see this page"
+        
+        msum [
+                dir "new-post" $ methodSP GET $ do
+                    renderPage dbh layout "admin-new-post" []
+                ,
+                dir "new-post" $ methodSP POST $ newPost layout dbh,
+                methodSP GET $ renderPage dbh layout "admin" []
+            ]
+
+newPost :: DB.IConnection conn =>
+    Layout -> conn -> ServerPartT IO Response
+newPost layout dbh = do
+    body <- DB.toSql <$> ("body" `lookDefault` "")
+    title <- DB.toSql <$> ("title" `lookDefault` "")
+    timestamp <- liftIO $ DB.toSql
+        <$> formatTime defaultTimeLocale "%F %T"
+        <$> getCurrentTime
+    
+    pid <- liftIO $ do
+        DB.run dbh
+            "insert into posts (title, timestamp, body) values (?,?,?)"
+            [title, timestamp, body]
+        DB.commit dbh
+        DB.fromSql <$> head <$> fromJust 
+            <$> DB.rowList dbh "select last_insert_rowid()" []
+    found (blogRoot layout ++ "/posts/" ++ pid) $
+        toResponse "Forwarding to created post"
