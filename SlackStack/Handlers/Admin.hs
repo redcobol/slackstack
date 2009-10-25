f9cbd60c9ea6e81b272b85f2aa883804ce4a4b6b
o_O <substack@sunspot.(none)>
2009-10-24 23:51:38 -0800

    admin post edits

--- SlackStack/Handlers/Admin.hs
+++ SlackStack/Handlers/Admin.hs
@@ -14,6 +14,8 @@ import Data.Time.Clock (getCurrentTime)
 import Data.Time.Format
 import System.Locale (defaultTimeLocale)
 
+import qualified Data.Map as M
+
 adminHandlers :: DB.IConnection conn =>
     Layout -> conn -> ServerPartT IO Response
 adminHandlers layout dbh =
@@ -26,36 +28,60 @@ adminHandlers layout dbh =
             fail "Must be root-level to see this page"
         
         msum [
-                dir "edit-post" $ anyPath $ methodSP GET $ do
-                    postID <- head <$> rqPaths <$> askRq
-                    renderPage dbh layout "admin-edit-post"
-                        [ "postID" ==> postID ]
+                dir "edit-post" $ path $ \postID -> methodSP GET $ do
+                    rowM <- liftIO $ DB.rowMap dbh
+                        "select * from posts where id = ?"
+                        [DB.toSql postID]
+                    when (isNothing rowM) $ fail "No post by that id exists"
+                    let row = M.map DB.sqlAsString $ fromJust rowM
+                    
+                    renderPage dbh layout "admin-edit-post" [
+                            "postID" ==> (postID :: String),
+                            "postTitle" ==> row M.! "title",
+                            "postBody" ==> row M.! "body"
+                       ]
                 ,
+                dir "edit-post" $ methodSP POST $ editPost layout dbh,
                 dir "new-post" $ methodSP GET $ do
                     renderPage dbh layout "admin-new-post" []
                 ,
-                dir "new-post" $ methodSP POST $ newPost layout dbh,
+                dir "new-post" $ methodSP POST $ createNewPost layout dbh,
                 methodSP GET $ renderPage dbh layout "admin" []
             ]
 
-newPost :: DB.IConnection conn =>
+createNewPost :: DB.IConnection conn =>
     Layout -> conn -> ServerPartT IO Response
-newPost layout dbh = do
+createNewPost layout dbh = do
     body <- DB.toSql <$> ("body" `lookDefault` "")
     title <- DB.toSql <$> ("title" `lookDefault` "")
     timestamp <- liftIO $ DB.toSql
         <$> formatTime defaultTimeLocale "%F %T"
         <$> getCurrentTime
     
-    pid <- liftIO $ msum $ repeat $ do
-        pid' <- randHex 6
+    -- create random strings of hex until one of them happens to be unique
+    postID <- liftIO $ msum $ repeat $ do
+        postID' <- randHex 6
         DB.run dbh
             "insert into posts \
                 \ (id, title, timestamp, body) \
                 \ values (?,?,?,?)"
-            [DB.toSql pid', title, timestamp, body]
-        return pid'
+            [DB.toSql postID', title, timestamp, body]
+        return postID'
     liftIO $ DB.commit dbh
     
-    found (blogRoot layout ++ "/posts/" ++ pid) $
+    found (blogRoot layout ++ "/posts/" ++ postID) $
         toResponse "Forwarding to created post"
+
+editPost :: DB.IConnection conn =>
+    Layout -> conn -> ServerPartT IO Response
+editPost layout dbh = do
+    body <- DB.toSql <$> ("body" `lookDefault` "")
+    title <- DB.toSql <$> ("title" `lookDefault` "")
+    postID <- "post_id" `lookDefault` ""
+    
+    liftIO $ DB.run dbh
+        "update posts set title = ?, body = ? where id = ?"
+        [ title, body, DB.toSql postID ]
+    
+    found (blogRoot layout ++ "/posts/" ++ postID) $
+        toResponse "Forwarding to edited post"
